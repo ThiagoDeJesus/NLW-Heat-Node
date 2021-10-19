d7b924f313d57faf6b50e4edb5a9e8a9065475f5
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-19 18:50:41 -0300

    feat: cria feature para retornar 3 ultimas mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 
 const router = Router();
 
@@ -12,5 +13,7 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+
+router.get("/messages/last3", new GetLast3MessagesController().handle);
 
 export { router };
